e5cbf5ebfc0fc0d87e4177a1e27f20badf3dde26
Marie-Alice Blete <marie-alice.blete@worldline.com>
2024-04-01 15:22:40 +0200

    Fix image local persistence

--- src/main/java/_2_ImageGeneration.java
+++ src/main/java/_2_ImageGeneration.java
@@ -36,30 +36,13 @@ public class _2_ImageGeneration {
             // -----------------------
             // Assignment:
             //      - connect to OpenAi's Dall-E ImageModel and set some more parameters
-            //      - generate your image and store it in the root (file handling code provided)
+            //      - generate your image and persist it (use builder with .persistTo( ... ) )
 
             // TODO make an overview of the options in readme
             ImageModel model = null;
 
             Response<Image> response = model.generate("2 funny cats");
 
-            // TODO figure out why it's null
-            String base64Image = response.content().base64Data();
-            System.out.println(base64Image);
-
-          /*  byte[] imageBytes = Base64.getDecoder().decode(base64Image);
-            InputStream stream = new ByteArrayInputStream(imageBytes);
-
-            OutputStream out = null;
-            try {
-                out = Files.newOutputStream(Paths.get("aiImage.png"));
-                out.write(imageBytes);
-                out.close();
-            } catch (IOException e) {
-                throw new RuntimeException("Error when writing image to disk: " + e);
-            } */
-
-            // TODO remove this method and check if always stored locally or only on .url()
             System.out.println(response.content().url());
         }
     }
